af2f271fa303efde17043a0d5784eec5af838d98
quinti <quintitecnologias@gmail.com>
2026-05-12 03:10:13 -0300

    mudanças pequenas

--- colorjorge-react/src/screens/board/BoardMobile.js
+++ colorjorge-react/src/screens/board/BoardMobile.js
@@ -2,6 +2,15 @@ import React, { useState } from 'react';
 import Sketch from 'react-p5';
 import ColorBoard from '../../components/game/ColorBoard';
 
+
+function BoardMobile() {
+    return (
+        <div>
+            {/* código da sua interface mobile */}
+        </div>
+    );
+}
+
 <div>
     <ColorBoard/>
 </div>
